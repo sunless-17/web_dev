b83cc416779d5077bf82c2908bd9c18647caf415
Sunless <sunless17@tutamail.com>
2025-04-04 20:06:10 +0300

    feat: add html elements
    helix bug, not formatting and treesitter not working properly when
    working with dioxus

--- hot_dog/src/main.rs
+++ hot_dog/src/main.rs
@@ -6,5 +6,16 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    rsx!("hello")
+    rsx!(
+        div { id: "title",
+            h1 { "HotDog! 🌭" }
+        }
+        div { id: "dogview",
+            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+        }
+        div { id: "buttons",
+            button { id: "skip", "skip" }
+            button { id: "save", "save!" }
+        }
+    )
 }
